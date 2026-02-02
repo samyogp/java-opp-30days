cb941244bf85698a7c80e0f3d4c403310052eddc
samyogp <samyogphuyal036@gmail.com>
2026-02-02 16:46:10 +0545

    adding first program of complete code with prashant sir tutorials

---
+++ day07_tutorial/coadingbyprashantsir/firstprogram.java
@@ -0,0 +1,9 @@
+package day07_tutorial.coadingbyprashantsir;
+
+
+public class firstprogram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to phuyal creates java tutorials!");
+    }
+    
+}
