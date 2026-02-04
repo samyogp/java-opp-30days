09b3198c7ad79ae4b6c87631074b30dd5e7abf9a
samyogp <samyogphuyal036@gmail.com>
2026-02-04 22:49:59 +0545

    person account demo using encapsulation

---
+++ day09_Encapsulation/AccountDemo.java
@@ -0,0 +1,17 @@
+package day09_Encapsulation;
+
+// encapsulation example of person account 
+
+public class AccountDemo {
+
+    private double balance;
+    public void setbalance (double b) {
+        balance = b;
+
+    }
+    private double getbalance() {
+        return balance;
+        
+    }
+    
+}
