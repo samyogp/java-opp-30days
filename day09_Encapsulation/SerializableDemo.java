94b67c747967044980d05da8ebf3ae9ba21e1303
samyogp <samyogphuyal036@gmail.com>
2026-02-05 00:22:21 +0545

    student class serializable and implements inn encapsulation in java

---
+++ day09_Encapsulation/SerializableDemo.java
@@ -0,0 +1,44 @@
+package day09_Encapsulation;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class SerializableDemo {
+
+    // Student class (inside same file)
+    static class Student implements Serializable {
+        private int roll;
+        private String name;
+
+        public Student(int roll, String name) {
+            this.roll = roll;
+            this.name = name;
+        }
+
+        public int getRoll() {
+            return roll;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    // main method
+    public static void main(String[] args) {
+        try {
+            Student s = new Student(101, "Ram");
+
+            ObjectOutputStream out =
+                    new ObjectOutputStream(new FileOutputStream("student.ser"));
+
+            out.writeObject(s);
+            out.close();
+
+            System.out.println("Student object saved successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
