5653b80d5bae9d4cbc81c3d889f8b0f44fe49a4d
samyogp <samyogphuyal036@gmail.com>
2026-02-04 22:45:42 +0545

    Encapsulation test demo added

---
+++ day09_Encapsulation/TestStudentDemo.java
@@ -0,0 +1,10 @@
+package day09_Encapsulation;
+
+public class TestStudentDemo {
+    public static void main(String[] args) {
+        student s1 = new student();
+        s1.setRoll(101);
+        System.out.println("Student roll number: " + s1.getRoll());
+    }
+    
+}
---
+++ day09_Encapsulation/student.java
@@ -0,0 +1,16 @@
+package day09_Encapsulation;
+
+
+// Encapsulation example
+public class student {
+    private int roll;
+    public void setRoll(int r) {
+        roll = r;
+    }
+    public int getRoll() {
+        return roll;
+        
+    }
+
+    
+}
