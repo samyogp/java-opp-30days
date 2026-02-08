c3ad44bb756fa2af2eb2340998bc66d49738ff36
samyogp <samyogphuyal036@gmail.com>
2026-02-08 09:02:28 +0545

    Arithmetic operations demo revision added

---
+++ day10_revision/ArithmetcOperations.java
@@ -0,0 +1,20 @@
+package day10_revision;
+
+public class ArithmetcOperations {
+    public static void main(String[] args) {
+        int  answer = 2 +2;
+        System.out.println(answer);
+        answer = answer -1;
+        System.out.println(answer);
+        answer = answer *2;
+        System.out.println(answer);
+        answer = answer /2;
+        System.out.println(answer);
+        answer = answer +8;
+        System.out.println(answer);
+        answer = answer %7;
+        System.out.println(answer);
+        
+    }
+    
+}
