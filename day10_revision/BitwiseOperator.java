19fe3d509c5a94babe0b77f66b9ce1d4a46aee3c
samyogp <samyogphuyal036@gmail.com>
2026-02-08 09:20:14 +0545

    add bitwise operator demo

---
+++ day10_revision/BitwiseOperator.java
@@ -0,0 +1,20 @@
+package day10_revision;
+
+public class BitwiseOperator {
+
+    public static void main(String[] args) {
+        int A = 10; // 1010 in binary
+        int B = 3;  // 0011 in binary
+        int Y;
+        Y = A & B;
+        System.out.println(Y); 
+        Y = A | B;
+        System.out.println(Y);
+        Y = A ^ B;
+        System.out.println(Y);
+
+
+
+    }
+    
+}
