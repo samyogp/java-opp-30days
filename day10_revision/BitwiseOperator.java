11b538c82968175857518c106a2c8b4b5f10f93b
samyogp <samyogphuyal036@gmail.com>
2026-02-08 09:21:09 +0545

    comment added in bitwise operator demo

--- day10_revision/BitwiseOperator.java
+++ day10_revision/BitwiseOperator.java
@@ -7,9 +7,9 @@ public class BitwiseOperator {
         int B = 3;  // 0011 in binary
         int Y;
         Y = A & B;
-        System.out.println(Y); 
+        System.out.println(Y); // Output: 2 (0010 in binary)
         Y = A | B;
-        System.out.println(Y);
+        System.out.println(Y); // Output: 11 (1011 in binary)
         Y = A ^ B;
         System.out.println(Y);
 
