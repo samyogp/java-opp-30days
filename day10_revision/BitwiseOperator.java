b67fce3fc90a58abf9b64dcf0a651c995b11e029
samyogp <samyogphuyal036@gmail.com>
2026-02-08 09:22:49 +0545

    another comment added in bitwiseoperator ecample code

--- day10_revision/BitwiseOperator.java
+++ day10_revision/BitwiseOperator.java
@@ -6,11 +6,11 @@ public class BitwiseOperator {
         int A = 10; // 1010 in binary
         int B = 3;  // 0011 in binary
         int Y;
-        Y = A & B;
+        Y = A & B; // Output: 2 (0010 in binary)
         System.out.println(Y); // Output: 2 (0010 in binary)
-        Y = A | B;
+        Y = A | B; // Output: 11 (1011 in binary)
         System.out.println(Y); // Output: 11 (1011 in binary)
-        Y = A ^ B;
+        Y = A ^ B; // Output: 9 (1001 in binary)
         System.out.println(Y);
 
 
