707472494aa73170e9dc75a954e6342820a311d3
samyogp <samyogphuyal036@gmail.com>
2026-02-08 09:13:31 +0545

    adding another program examles o relational operator with using integager  a and b

--- day10_revision/Relationaloperator.java
+++ day10_revision/Relationaloperator.java
@@ -3,7 +3,7 @@ package day10_revision;
 public class Relationaloperator {
 
     public static void main(String args[]) {
-        int p = 5;
+        /* int p = 5;
         int q = 10;
         System.out.println("p == q: " + (p == q)); // false
         System.out.println("p != q: " + (p != q)); // true
@@ -11,8 +11,17 @@ public class Relationaloperator {
         System.out.println("p < q: " + (p < q));   // true
         System.out.println("p >= q: " + (p >= q)); // false
         System.out.println("p <= q: " + (p <= q)); // true
-        // You can change the values of p and q to see different results
+        // You can change the values of p and q to see different results */
 
+        int a = 15;
+        int b = 20;
+        System.out.println("a == b: " + (a == b)); 
+        System.out.println("a !=b: " + (a !=b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a <b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+        
     }
     
 }
