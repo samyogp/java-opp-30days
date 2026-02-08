8cf41e35c8066c7eee22d4727fc6c55fea06edd0
samyogp <samyogphuyal036@gmail.com>
2026-02-08 09:14:50 +0545

    adding comment

--- day10_revision/ArithmetcOperations.java
+++ day10_revision/ArithmetcOperations.java
@@ -4,12 +4,15 @@ package day10_revision;
 // Arithmetic operations demo examples revising addition, subtraction, multiplication, division, and modulus.
 
 public class ArithmetcOperations {
+    // main method
+
     public static void main(String[] args) {
         int  answer = 2 +2;
         System.out.println(answer);
         answer = answer -1;
         System.out.println(answer);
         answer = answer *2;
+        // print the answer
         System.out.println(answer);
         answer = answer /2;
         System.out.println(answer);
