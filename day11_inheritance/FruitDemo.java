ab90a35493e59eadfe5e2b9f32c51970e5805892
samyogp <samyogphuyal036@gmail.com>
2026-02-12 14:27:20 +0545

    adding comments on fruitdemo

--- day11_inheritance/FruitDemo.java
+++ day11_inheritance/FruitDemo.java
@@ -1,23 +1,29 @@
 package day11_inheritance;
 
+// parent class
+
 class fruit {
     void taste() {
         System.out.println("fruit is sweet");
 
     }
 }
+
+// child class
  class Mango extends fruit {
     void Color() {
         System.out.println("mango is yellow in color");
 
     }
  }
+
+ // main class
 public class FruitDemo {
     public static void main(String[] args) {
         Mango m = new Mango();
         m.taste();
         m.Color();
-        
+
     }
     
 }
