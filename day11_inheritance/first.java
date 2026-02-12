12bd70685cd7c01695211b7762404eee5442143e
samyogp <samyogphuyal036@gmail.com>
2026-02-12 13:47:39 +0545

    added day11_inheritance and example of the animal

---
+++ day11_inheritance/first.java
@@ -0,0 +1,25 @@
+package day11_inheritance;
+ // inheritance 
+// One class can use the properties and methods of another class.
+
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+
+public class first {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+
+        d.eat();   
+        d.bark();  
+    }
+    }
+
