4ae13ec44f3f9f6f8db3331463e03e04afe92fd5
samyogp <samyogphuyal036@gmail.com>
2026-02-14 07:59:45 +0545

    adding main method on example 2

--- day12_polymorphism/CompileTimePolymorphism/Example2.java
+++ day12_polymorphism/CompileTimePolymorphism/Example2.java
@@ -1,6 +1,6 @@
 package day12_polymorphism.CompileTimePolymorphism;
 
-public class printer {
+public class Example2 {
 
     void print(int num) {
         System.out.println("integer: " + num);
@@ -9,5 +9,11 @@ public class printer {
     
     void print(String text) {
         System.out.println("string: " + text);
+    }
+    public static void main(String[] args) {
+        Example2 obj = new Example2();
+        obj.print(10);
+        obj.print("Hello");
+
     }
 }
