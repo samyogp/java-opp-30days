371273bfbe4675238f6ff3d68016529a15c06baf
samyogp <samyogphuyal036@gmail.com>
2026-02-14 08:02:03 +0545

    adding single line comments

--- day12_polymorphism/CompileTimePolymorphism/Example2.java
+++ day12_polymorphism/CompileTimePolymorphism/Example2.java
@@ -1,15 +1,17 @@
 package day12_polymorphism.CompileTimePolymorphism;
-
+// different data types of parameters
 public class Example2 {
 
     void print(int num) {
         System.out.println("integer: " + num);
 
     }
-    
+    // method overloading with different data types of parameters 
     void print(String text) {
         System.out.println("string: " + text);
     }
+    // main method to test the method overloading with different dat types of parameters
+    
     public static void main(String[] args) {
         Example2 obj = new Example2();
         obj.print(10);
