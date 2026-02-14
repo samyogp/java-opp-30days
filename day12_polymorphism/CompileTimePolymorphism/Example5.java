70d544363a52b07a4d76e36b1e366cfcd36dde32
samyogp <samyogphuyal036@gmail.com>
2026-02-14 08:20:32 +0545

    single line comments are added

--- day12_polymorphism/CompileTimePolymorphism/Example5.java
+++ day12_polymorphism/CompileTimePolymorphism/Example5.java
@@ -1,13 +1,15 @@
 package day12_polymorphism.CompileTimePolymorphism;
-
+ // static method overloading 
 public class Example5 {
      static int multiply(int a, int b) {
         return a * b;
     }
-
+  // method overloading with different data types of parameters
     static double multiply(double a, double b) {
         return a * b;
     }
+
+    // main method to test the static method overloading 
 
     public static void main(String[] args) {
         System.out.println(multiply(3, 4));
