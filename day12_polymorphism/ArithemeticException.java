f46432558751b8a15c0b74c87db1f45fa77d7216
samyogp <samyogphuyal036@gmail.com>
2026-02-15 22:35:06 +0545

    arithmetic exception

---
+++ day12_polymorphism/ArithemeticException.java
@@ -0,0 +1,15 @@
+package day12_polymorphism;
+
+public class ArithemeticException {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        try {
+            int c = a / b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
+
--- day12_polymorphism/nullobjectcall.java
+++ day12_polymorphism/nullobjectcall.java
@@ -1,12 +1,12 @@
 package day12_polymorphism;
 
-
+// Null Object Call Example 
 class Animal {
     void sound() {
         System.out.println("Sound");
     }
 }
-
+    // main class to test the null object call
 public class nullobjectcall {
     public static void main(String[] args) {
         Animal a = null;
